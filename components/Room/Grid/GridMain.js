34a6a412d527b013642de00ca9b8d0c282d92b86
govindgovind852 <govindgovind852@gmail.com>
2023-01-30 11:27:45 +0530

    starting with more changes

--- components/Room/Grid/GridMain.js
+++ components/Room/Grid/GridMain.js
@@ -12,6 +12,7 @@ import {
 import { GALLERY, SPEAKER } from "../../../Utils/Contants/Conditional";
 import GalleryView from "./GalleryView";
 import SpeakerView from "./SpeakerView";
+import Boxes from "./TempView";
 
 export default function GridMain({ profilename, audio, video }) {
   const userData = useSelector((s) => s.user.data);
---
+++ components/Room/Grid/VidePlayer.js
@@ -0,0 +1,13 @@
+import { Grid } from "@mui/material";
+
+export default function VideoPlayer() {
+  return (
+    <Grid
+      container
+      sx={{
+        width: "100%",
+        height: "100%",
+      }}
+    ></Grid>
+  );
+}
